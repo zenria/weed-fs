1bed45e568656ce90fa10990be86d0899c498a1a
Chris Lu <chris.lu@gmail.com>
2013-12-09 13:53:24 -0800

    toughen up error handling for invalid fid

--- go/weed/weed_server/volume_server_handlers.go
+++ go/weed/weed_server/volume_server_handlers.go
@@ -92,7 +92,11 @@ func (vs *VolumeServer) GetOrHeadHandler(w http.ResponseWriter, r *http.Request,
 		debug("parsing error:", err, r.URL.Path)
 		return
 	}
-	n.ParsePath(fid)
+	err = n.ParsePath(fid)
+	if err != nil {
+		glog.V(2).Infoln("parsing fid error:", err, r.URL.Path)
+		return
+	}
 
 	debug("volume", volumeId, "reading", n)
 	if !vs.store.HasVolume(volumeId) {
